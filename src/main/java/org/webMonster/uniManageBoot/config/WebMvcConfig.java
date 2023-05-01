8a4995c04a11c513426a357e8c02c90e15ad5866
JonghaLee <jhl9617@gmail.com>
2023-05-01 17:30:44 +0900

    addResourceHandlers 추가 새로고침 했을 때 에러페이지 출력하는 오류 수정

--- src/main/java/org/webMonster/uniManageBoot/config/WebMvcConfig.java
+++ src/main/java/org/webMonster/uniManageBoot/config/WebMvcConfig.java
@@ -1,11 +1,17 @@
 package org.webMonster.uniManageBoot.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
 import org.springframework.data.web.SortHandlerMethodArgumentResolver;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
+
+import java.io.IOException;
 import java.util.List;
 
 @Configuration
@@ -21,6 +27,21 @@ public class WebMvcConfig implements WebMvcConfigurer {
         pageableArgumentResolver.setMaxPageSize(500);
         pageableArgumentResolver.setFallbackPageable(PageRequest.of(0, 10));
         argumentResolvers.add(pageableArgumentResolver);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/static/")
+                .resourceChain(true)
+                .addResolver(new PathResourceResolver() {
+                    @Override
+                    protected Resource getResource(String resourcePath, Resource location) throws IOException {
+                        Resource requestedResource = location.createRelative(resourcePath);
+                        return requestedResource.exists() && requestedResource.isReadable() ? requestedResource
+                                : new ClassPathResource("/static/index.html");
+                    }
+                });
     }
 }
 
