2e7674c996bbc7563601137d682a6b82d8a69905
moharago <yuna922@naver.com>
2023-05-02 17:20:55 +0900

    전체 entity 추가

---
+++ src/main/generated/org/webMonster/uniManageBoot/member/entity/QMemberEntity.java
@@ -0,0 +1,59 @@
+package org.webMonster.uniManageBoot.member.entity;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+
+
+/**
+ * QMemberEntity is a Querydsl query type for MemberEntity
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QMemberEntity extends EntityPathBase<MemberEntity> {
+
+    private static final long serialVersionUID = -1823131047L;
+
+    public static final QMemberEntity memberEntity = new QMemberEntity("memberEntity");
+
+    public final StringPath address1 = createString("address1");
+
+    public final StringPath address2 = createString("address2");
+
+    public final NumberPath<Integer> auth = createNumber("auth", Integer.class);
+
+    public final DatePath<java.sql.Date> birthday = createDate("birthday", java.sql.Date.class);
+
+    public final StringPath department = createString("department");
+
+    public final StringPath email = createString("email");
+
+    public final NumberPath<Integer> grade = createNumber("grade", Integer.class);
+
+    public final NumberPath<Integer> member_id = createNumber("member_id", Integer.class);
+
+    public final StringPath member_pwd = createString("member_pwd");
+
+    public final StringPath name = createString("name");
+
+    public final StringPath phone = createString("phone");
+
+    public final NumberPath<Integer> postcode = createNumber("postcode", Integer.class);
+
+    public QMemberEntity(String variable) {
+        super(MemberEntity.class, forVariable(variable));
+    }
+
+    public QMemberEntity(Path<? extends MemberEntity> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QMemberEntity(PathMetadata metadata) {
+        super(MemberEntity.class, metadata);
+    }
+
+}
+
