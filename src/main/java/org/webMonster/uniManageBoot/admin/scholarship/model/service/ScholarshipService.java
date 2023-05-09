d02b10b0d0a8fe72adbd16134866c3506be4af8b
JonghaLee <jhl9617@gmail.com>
2023-05-09 11:26:56 +0900

    카멜표기법 변경 누락 수정

--- src/main/java/org/webMonster/uniManageBoot/admin/scholarship/model/service/ScholarshipService.java
+++ src/main/java/org/webMonster/uniManageBoot/admin/scholarship/model/service/ScholarshipService.java
@@ -1,4 +1,11 @@
 package org.webMonster.uniManageBoot.admin.scholarship.model.service;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
 public class ScholarshipService {
 }
