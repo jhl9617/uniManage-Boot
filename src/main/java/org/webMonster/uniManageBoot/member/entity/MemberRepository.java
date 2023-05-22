4c54d2caa3975fd2b69d3c07650e1cde31a1f747
su jin <itsyksj@gmail.com>
2023-05-22 11:21:35 +0900

    임시

--- src/main/java/org/webMonster/uniManageBoot/member/entity/MemberRepository.java
+++ src/main/java/org/webMonster/uniManageBoot/member/entity/MemberRepository.java
@@ -8,8 +8,15 @@ import org.springframework.data.repository.query.Param;
 import org.webMonster.uniManageBoot.common.SearchCondition;
 import org.webMonster.uniManageBoot.professor.lecture.entity.LectureEntity;
 
+import java.util.Optional;
+
 
 public interface MemberRepository extends JpaRepository<MemberEntity, Long>, MemberRepositoryCustom {
+
+    //개인정보 조회(교수용)
+    @Query("SELECT m FROM MemberEntity m WHERE m.memberId =: memberId")
+    MemberEntity findAllById(@Param("memberId") long memberId);
+
     @Query("SELECT m FROM MemberEntity m WHERE m.memberId = :memberId AND m.memberPwd = :memberPwd")
     MemberEntity findByMemberIdAndMemberPwd(@Param("memberId") long memberId, @Param("memberPwd") String memberPwd);
 
