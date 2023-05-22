c08e8f4d4094319cd6b5bf94955c808b6788d497
moharago <yuna922@naver.com>
2023-05-22 18:15:29 +0900

    학생정보시스템 공지사항 리스트/상세보기 페이지 생성 및 출력, 사이드바 수정, 교직원 학사일정 리스트 페이지 생성 및 출력, 학생정보시스템 메인페이지 수정,

--- src/main/java/org/webMonster/uniManageBoot/admin/schedule/entity/ScheduleEntity.java
+++ src/main/java/org/webMonster/uniManageBoot/admin/schedule/entity/ScheduleEntity.java
@@ -1,12 +1,10 @@
 package org.webMonster.uniManageBoot.admin.schedule.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.*;
 import java.sql.Date;
+import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
@@ -14,9 +12,17 @@ import java.sql.Date;
 @Builder
 @Table(name = "SCHEDULE")  //Board 테이블 자동 생성시키는 어노테이션임
 @Entity
+@Setter
+@Getter
 public class ScheduleEntity {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sche_seq_gen")
+    @SequenceGenerator(
+            name = "sche_seq_gen",
+            sequenceName = "SCHE_ID_SEQ",
+            allocationSize=1
+    )
     @Column(name = "SCHE_ID")
     private long scheId;   //공지사항 번호
     @Column(name = "SCHE_TITLE")
@@ -24,9 +30,7 @@ public class ScheduleEntity {
     @Column(name = "SCHE_CONTENT")
     private String scheContent;   //공지사항 내용
     @Column(name = "START_DATE")
-    private Date startDate;   //작성자
+    private LocalDateTime startDate;   //작성자
     @Column(name = "END_DATE")
-    private Date endDate;   //등록일
-    @Column(name = "READCOUNT")
-    private int readcount;   //조회수
+    private LocalDateTime endDate;   //등록일
 }
