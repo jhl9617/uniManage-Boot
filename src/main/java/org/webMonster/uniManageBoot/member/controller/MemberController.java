76368e4b53cf0d447dc75e7541cbf1ae71434b3d
moharago <yuna922@naver.com>
2023-05-28 03:32:32 +0900

    학생,교직원 개인정보 수정 목록 변경

--- src/main/java/org/webMonster/uniManageBoot/member/controller/MemberController.java
+++ src/main/java/org/webMonster/uniManageBoot/member/controller/MemberController.java
@@ -188,6 +188,7 @@ public class MemberController {
     public MemberEntity updateStudentMypage(@RequestBody MemberDepartmentDto memberDepartmentDto, HttpSession session) {
         MemberDepartmentDto sessionMember = (MemberDepartmentDto) session.getAttribute("loginMember");
         sessionMember.setPhone(memberDepartmentDto.getPhone());
+        sessionMember.setEmail(memberDepartmentDto.getEmail());
         sessionMember.setPostcode(memberDepartmentDto.getPostcode());
         sessionMember.setAddress1(memberDepartmentDto.getAddress1());
         sessionMember.setAddress2(memberDepartmentDto.getAddress2());
@@ -197,6 +198,7 @@ public class MemberController {
         MemberEntity entity = memberRepository.findById(sessionMember.getMemberIdx()).orElse(null);
         if (entity != null) {
             entity.setPhone(sessionMember.getPhone());
+            entity.setEmail(sessionMember.getEmail());
             entity.setPostcode(sessionMember.getPostcode());
             entity.setAddress1(sessionMember.getAddress1());
             entity.setAddress2(sessionMember.getAddress2());
@@ -217,6 +219,7 @@ public class MemberController {
     public MemberEntity updateAdminMypage(@RequestBody MemberDepartmentDto memberDepartmentDto, HttpSession session) {
         MemberDepartmentDto sessionMember = (MemberDepartmentDto) session.getAttribute("loginMember");
         sessionMember.setPhone(memberDepartmentDto.getPhone());
+        sessionMember.setEmail(memberDepartmentDto.getEmail());
         sessionMember.setPostcode(memberDepartmentDto.getPostcode());
         sessionMember.setAddress1(memberDepartmentDto.getAddress1());
         sessionMember.setAddress2(memberDepartmentDto.getAddress2());
@@ -226,6 +229,7 @@ public class MemberController {
         MemberEntity entity = memberRepository.findById(sessionMember.getMemberIdx()).orElse(null);
         if (entity != null) {
             entity.setPhone(sessionMember.getPhone());
+            entity.setEmail(sessionMember.getEmail());
             entity.setPostcode(sessionMember.getPostcode());
             entity.setAddress1(sessionMember.getAddress1());
             entity.setAddress2(sessionMember.getAddress2());
